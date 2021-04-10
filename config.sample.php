a5de17fe413e63216a48fb248e2dda40aaf48761
Garnet DeGelder <garnet@garnetdg.ca>
2021-04-10 18:58:08 -0400

    Add sample config file

---
+++ config.sample.php
@@ -0,0 +1,8 @@
+<?php
+
+// backup database file
+// must be empty, readable and writable, and in a readable and writable directory
+$config['backup_database'] = 'backup.sqlite3';
+
+// Debug mode
+$config['debug'] = false;

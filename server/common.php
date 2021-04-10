56bfd1dd934fce99c3d3223191223d7779694d96
Garnet DeGelder <garnet@garnetdg.ca>
2021-04-10 19:28:18 -0400

    Use functions in common.php

--- server/common.php
+++ server/common.php
@@ -1,12 +1,23 @@
 <?php
 
-// load config
-$config = [];
-require('server_config.php');
+function load_config()
+{
+	global $config;
 
-// enable debug mode
-if (isset($config['debug']) && $config['debug']) {
-	error_reporting(E_ALL);
-	ini_set('display_errors', 'On');
+	// load config
+	$config = [];
+	require('config.php');
+
+	// enable debug mode
+	if (isset($config['debug']) && $config['debug']) {
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'On');
+	}
 }
 
+function db_connect()
+{
+	global $dbconn;
+
+
+}
